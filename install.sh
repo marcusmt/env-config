52d8604e0310a84121dd1116f241d85e20ae5ff4
Marcus Sousa <sousa.mmta@gmail.com>
2025-02-19 22:51:54 +0100

    creates install script and update readme with instructions to run it

---
+++ install.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+
+# Configure APT repositories
+ppa_list=(
+  "ppa:git-core/ppa"
+  "ppa:graphics-drivers/ppa"
+  "ppa:papirus/papirus"
+  "universe"
+)
+
+repositories=(
+ "wezterm-fury https://apt.fury.io/wez/gpg.key https://apt.fury.io/wez/ * *"
+ "packages.microsoft https://packages.microsoft.com/keys/microsoft.asc https://packages.microsoft.com/repos/code stable main"
+)
+
+packages=(
+  "apt-transport-https"
+  "arandr"
+  "blueman"
+  "brightnessctl"
+  "build-essential"
+  "cmake"
+  "code"
+  "feh"
+  "fish"
+  "flameshot"
+  "git"
+  "i3"
+  "libfuse2"
+  "lxappearance"
+  "papirus-icon-theme"
+  "pasystray"
+  "pavucontrol"
+  "policykit-1-gnome"
+  "wezterm"
+  "zsh"
+)
+
+packages_picom=(
+  "libconfig-dev"
+  "libdbus-1-dev"
+  "libegl-dev"
+  "libepoxy-dev"
+  "libev-dev"
+  "libgl-dev"
+  "libpcre2-dev"
+  "libpixman-1-dev"
+  "libx11-xcb-dev"
+  "libxcb-composite0-dev"
+  "libxcb-damage0-dev"
+  "libxcb-glx0-dev"
+  "libxcb-image0-dev"
+  "libxcb-present-dev"
+  "libxcb-randr0-dev"
+  "libxcb-render-util0-dev"
+  "libxcb-render0-dev"
+  "libxcb-shape0-dev"
+  "libxcb-util-dev"
+  "libxcb-xfixes0-dev"
+  "libxcb1-dev"
+  "meson"
+  "ninja-build"
+  "uthash-dev"
+)
+
+packages_dunst=(
+  "libgdk-pixbuf-2.0-dev"
+  "libglib2.0-dev"
+  "libgtk-3-dev"
+  "libnotify-dev"
+  "libpango1.0-dev"
+  "librsvg2-dev"
+  "libx11-dev"
+  "libxdg-basedir-dev"
+  "libxinerama-dev"
+  "libxrandr-dev"
+  "libxss-dev"
+)
+
+for ppa in "${ppa_list[@]}"; do
+  sudo add-apt-repository -y "ppa:$ppa"
+done
+
+for repo in "${repositories[@]}"; do
+    IFS=' ' read -r name key_url source_parts <<< "$repo"
+    sudo wget -qO - $key_url | sudo gpg --yes --dearmor -o "/etc/apt/keyrings/${name}.gpg"
+    echo "deb [signed-by=/etc/apt/keyrings/${name}.gpg] $source_parts" | sudo tee "/etc/apt/sources.list.d/${name}.list" > /dev/null
+done
+
+sudo apt --purge remove -y '*nvidia*'
+sudo snap remove firefox
+sudo apt remove -y firefox gnome-terminal gnome-text-editor
+
+# System update
+sudo apt update -y && sudo apt upgrade -y && sudo ubuntu-drivers install && snap-store --quit && sudo snap refresh snap-store
+
+# Install packages
+sudo apt install -y "${packages[@]}" "${packages_picom[@]}" "${packages_dunst[@]}"
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
+sudo apt install -y ./chrome.deb
+rm -rf chrome.deb
+
+wget -O nvim-linux-x86_64.tar.gz https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+sudo mv /opt/nvim-linux-x86_64 /opt/nvim
+
+wget -qO - https://starship.rs/install.sh | sh -s -- -y
+
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+$HOME/.fzf/install --all --no-fish
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Hack.zip
+mkdir ~/.fonts
+unzip Hack.zip -d ~/.fonts
+fc-cache -fv
+rm Hack.zip
+
+# Picom
+cd ~/Downloads
+git clone https://github.com/yshui/picom.git
+cd picom
+meson setup --buildtype=release build
+ninja -C build
+sudo ninja -C build install
+
+# Dunst
+cd ~/Downloads
+git clone https://github.com/dunst-project/dunst.git
+cd dunst
+make
+sudo make install
+
+sudo apt autoremove -y
+
+echo "Xft.dpi: 192" | tee .Xresources
+
+sudo usermod -aG video ${USER}
+
+# My Dots
+wget -O ~/Pictures/wall.jpg https://gruvbox-wallpapers.pages.dev/wallpapers/irl/kace-rodriguez-p3OzJuT_Dks.jpg
+#cd ~/Downloads/
+#cp -r env-config-ubuntu-awesomewm/i3 ~/.config/
+#cp -r env-config-ubuntu-awesomewm/picom/ ~/.config/
+#cp -r env-config-ubuntu-awesomewm/gtk-3.0/ ~/.config/
+#cp -r env-config-ubuntu-awesomewm/gtk-4.0/ ~/.config/
+#cp env-config-ubuntu-awesomewm/.wezterm.lua ~/
+sudo sed -i "s/^GTK_THEME=.*$/GTK_THEME=\"Adwaita-dark\"/" /etc/environment || sudo sed -i "\$aGTK_THEME=\"Adwaita-dark\"" /etc/environment
