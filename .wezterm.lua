2124a60006e9745a138cfd1341a3ac9086c6539a
Marcus Sousa <sousa.mmta@gmail.com>
2025-02-22 22:28:43 +0100

    update Wezterm and install script

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,19 +4,22 @@ local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
-config.default_prog = { 'zsh', '-l' }
+config.default_prog = { 'fish', '-l' }
 
 -- This is where you actually apply your config choices
 
 config.keys = {
     {
-        key = 'w',
-        mods = 'ALT',
-        action = wezterm.action.CloseCurrentPane { confirm = false },
+        key = 'RightArrow',
+        mods = 'CTRL|SHIFT',
+        action = wezterm.action.SplitPane {
+            direction = 'Right',
+            size = { Percent = 50 },
+        },
     },
     {
-        key = 'RightArrow',
-        mods = 'ALT|SHIFT',
+        key = 'LeftArrow',
+        mods = 'CTRL|SHIFT',
         action = wezterm.action.SplitPane {
             direction = 'Left',
             size = { Percent = 50 },
@@ -24,50 +27,63 @@ config.keys = {
     },
     {
         key = 'DownArrow',
-        mods = 'ALT|SHIFT',
+        mods = 'CTRL|SHIFT',
         action = wezterm.action.SplitPane {
             direction = 'Down',
             size = { Percent = 50 },
         },
     },
     {
+        key = 'UpArrow',
+        mods = 'CTRL|SHIFT',
+        action = wezterm.action.SplitPane {
+            direction = 'Up',
+            size = { Percent = 50 },
+        },
+    },
+    {
         key = 'LeftArrow',
-        mods = 'ALT',
+        mods = 'CTRL',
         action = act.ActivatePaneDirection 'Left',
     },
     {
         key = 'RightArrow',
-        mods = 'ALT',
+        mods = 'CTRL',
         action = act.ActivatePaneDirection 'Right',
     },
     {
         key = 'UpArrow',
-        mods = 'ALT',
+        mods = 'CTRL',
         action = act.ActivatePaneDirection 'Up',
     },
     {
         key = 'DownArrow',
-        mods = 'ALT',
+        mods = 'CTRL',
         action = act.ActivatePaneDirection 'Down',
     },
     {
-        key = 't',
-        mods = 'ALT',
+        key = 'e',
+        mods = 'CTRL',
         action = act.SpawnTab 'DefaultDomain'
     },
     {
         key = 'w',
-        mods = 'ALT|SHIFT',
+        mods = 'CTRL',
+        action = wezterm.action.CloseCurrentPane { confirm = false },
+    },
+    {
+        key = 'w',
+        mods = 'CTRL|SHIFT',
         action = wezterm.action.CloseCurrentTab { confirm = true },
     },
     {
-        key = '[',
-        mods = 'ALT',
+        key = 'a',
+        mods = 'CTRL',
         action = act.MoveTabRelative(-1)
     },
     {
-        key = ']',
-        mods = 'ALT',
+        key = 'd',
+        mods = 'CTRL',
         action = act.MoveTabRelative(1)
     },
 }
@@ -76,13 +92,17 @@ for i = 1, 8 do
     -- CTRL+ALT + number to move to that position
     table.insert(config.keys, {
         key = tostring(i),
-        mods = 'ALT|SHIFT',
+        mods = 'CTRL|SHIFT',
         action = wezterm.action.MoveTab(i - 1),
     })
 end
 
 -- For example, changing the color scheme:
 config.color_scheme = 'ayu'
+config.enable_scroll_bar = true
+config.pane_focus_follows_mouse = true
+config.scrollback_lines = 9999999
+config.tab_bar_at_bottom = true
 
 -- and finally, return the configuration to wezterm
 return config
--- install.sh
+++ install.sh
@@ -110,18 +110,19 @@ sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 sudo mv /opt/nvim-linux-x86_64 /opt/nvim
 
 wget -qO - https://starship.rs/install.sh | sh -s -- -y
+echo "starship init fish | source" >> $HOME/.config/fish/config.fish
 
 git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
 $HOME/.fzf/install --all
 
 wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Hack.zip
-mkdir ~/.fonts
-unzip Hack.zip -d ~/.fonts
+mkdir $HOME/.fonts
+unzip Hack.zip -d $HOME/.fonts
 fc-cache -fv
 rm Hack.zip
 
 # Picom
-cd ~/Downloads
+cd $HOME/Downloads
 git clone https://github.com/yshui/picom.git
 cd picom
 meson setup --buildtype=release build
@@ -129,7 +130,7 @@ ninja -C build
 sudo ninja -C build install
 
 # Dunst
-cd ~/Downloads
+cd $HOME/Downloads
 git clone https://github.com/dunst-project/dunst.git
 cd dunst
 make
@@ -139,17 +140,17 @@ sudo snap remove firefox
 sudo apt remove -y firefox gnome-terminal gnome-text-editor dunst i3lock xss-lock
 sudo apt autoremove -y
 
-echo "Xft.dpi: 192" | tee ~/.Xresources
+echo "Xft.dpi: 192" | tee $HOME/.Xresources
 
 sudo usermod -aG video ${USER}
 
 # My Dots
-wget -O ~/Pictures/wall.jpg https://gruvbox-wallpapers.pages.dev/wallpapers/irl/kace-rodriguez-p3OzJuT_Dks.jpg
-cd ~/Downloads/
-cp -r env-config-ubuntu-i3/i3 ~/.config/
-cp -r env-config-ubuntu-i3/picom/ ~/.config/
-cp -r env-config-ubuntu-i3/dunst/ ~/.config/
-cp -r env-config-ubuntu-i3/gtk-3.0/ ~/.config/
-cp -r env-config-ubuntu-i3/gtk-4.0/ ~/.config/
-cp env-config-ubuntu-i3/.wezterm.lua ~/
+wget -O $HOME/Pictures/wall.jpg https://gruvbox-wallpapers.pages.dev/wallpapers/irl/kace-rodriguez-p3OzJuT_Dks.jpg
+cd $HOME/Downloads/
+cp -r env-config-ubuntu-i3/i3 $HOME/.config/
+cp -r env-config-ubuntu-i3/picom/ $HOME/.config/
+cp -r env-config-ubuntu-i3/dunst/ $HOME/.config/
+cp -r env-config-ubuntu-i3/gtk-3.0/ $HOME/.config/
+cp -r env-config-ubuntu-i3/gtk-4.0/ $HOME/.config/
+cp env-config-ubuntu-i3/.wezterm.lua $HOME/
 sudo sed -i "s/^GTK_THEME=.*$/GTK_THEME=\"Adwaita-dark\"/" /etc/environment || sudo sed -i "\$aGTK_THEME=\"Adwaita-dark\"" /etc/environment
